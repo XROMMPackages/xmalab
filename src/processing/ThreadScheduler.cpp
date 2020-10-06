63999e0b77780282e06d28f777184503cc99ca28
Benjamin Knorlein <Benjamin_Knorlein@brown.edu>
2020-10-06 11:36:57 -0400

    fixes #232 : fixed a typo

--- src/processing/ThreadScheduler.cpp
+++ src/processing/ThreadScheduler.cpp
@@ -105,7 +105,7 @@ void ThreadScheduler::finalize_updateTrialData()
 		}
 		trial->setRequiresRecomputation(false);
 	}
-	trials->clear();
+	trials.clear();
 	running = false;
 	MainWindow::getInstance()->redrawGL();
 }
